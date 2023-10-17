387721d663369e337d7e9a081114e30709fc96fd
MONO ᵈᵉᵛ <68543161+Mono-94@users.noreply.github.com>
2023-10-17 19:03:23 +0200

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,9 +1,7 @@
-
-
 CREATE TABLE `owned_vehicles` (
   `amigos` longtext DEFAULT NULL,
   `owner` varchar(46) DEFAULT NULL,
-  `plate` varchar(12) NOT NULL, 
+  `plate` varchar(12) NOT NULL,
   `vehicle` longtext DEFAULT NULL,
   `type` varchar(20) NOT NULL DEFAULT 'car',
   `job` varchar(20) DEFAULT NULL,
@@ -13,8 +11,19 @@ CREATE TABLE `owned_vehicles` (
   `mileage` int(11) DEFAULT 0,
   `glovebox` longtext DEFAULT NULL,
   `trunk` longtext DEFAULT NULL,
-  `lastparking` longtext DEFAULT NULL,
-  `lastposition` longtext DEFAULT NULL,
   `carseller` int(11) DEFAULT 0,
-  `infoimpound` longtext DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci; 
+  `calle` varchar(46) DEFAULT NULL,
+  `infoimpound` longtext DEFAULT NULL,
+  `lastparking` longtext DEFAULT NULL,
+  `lastposition` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+ALTER TABLE `owned_vehicles`
+  ADD PRIMARY KEY (`plate`),
+  ADD KEY `amigos` (`amigos`(1024)),
+  ADD KEY `amigos_2` (`amigos`(1024));
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
